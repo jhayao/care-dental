986814831b2489ac0720c5007ac1db2571f0a86f
Dyllj <wtautoan73@gmail.com>
2025-12-30 19:43:40 +0800

    chore: Remove unused database table creation statements for various system and application tables.

--- script.sql
+++ script.sql
@@ -1,13 +1,4 @@
-create table booking_fees
-(
-    id          bigint unsigned auto_increment
-        primary key,
-    booking_fee decimal(10, 2)              not null,
-    status      enum ('Active', 'Inactive') not null,
-    created_at  timestamp                   null,
-    updated_at  timestamp                   null
-)
-    collate = utf8mb4_unicode_ci;
+
 
 create table booking_items
 (
@@ -20,88 +11,11 @@ create table booking_items
 )
     collate = utf8mb4_unicode_ci;
 
-create table cache
-(
-    `key`      varchar(255) not null
-        primary key,
-    value      mediumtext   not null,
-    expiration int          not null
-)
-    collate = utf8mb4_unicode_ci;
 
-create table cache_locks
-(
-    `key`      varchar(255) not null
-        primary key,
-    owner      varchar(255) not null,
-    expiration int          not null
-)
-    collate = utf8mb4_unicode_ci;
 
-create table failed_jobs
-(
-    id         bigint unsigned auto_increment
-        primary key,
-    uuid       varchar(255)                          not null,
-    connection text                                  not null,
-    queue      text                                  not null,
-    payload    longtext                              not null,
-    exception  longtext                              not null,
-    failed_at  timestamp default current_timestamp() not null,
-    constraint failed_jobs_uuid_unique
-        unique (uuid)
-)
-    collate = utf8mb4_unicode_ci;
 
-create table job_batches
-(
-    id             varchar(255) not null
-        primary key,
-    name           varchar(255) not null,
-    total_jobs     int          not null,
-    pending_jobs   int          not null,
-    failed_jobs    int          not null,
-    failed_job_ids longtext     not null,
-    options        mediumtext   null,
-    cancelled_at   int          null,
-    created_at     int          not null,
-    finished_at    int          null
-)
-    collate = utf8mb4_unicode_ci;
 
-create table jobs
-(
-    id           bigint unsigned auto_increment
-        primary key,
-    queue        varchar(255)     not null,
-    payload      longtext         not null,
-    attempts     tinyint unsigned not null,
-    reserved_at  int unsigned     null,
-    available_at int unsigned     not null,
-    created_at   int unsigned     not null
-)
-    collate = utf8mb4_unicode_ci;
 
-create index jobs_queue_index
-    on jobs (queue);
-
-create table medical_records
-(
-    id         bigint unsigned auto_increment
-        primary key,
-    created_at timestamp null,
-    updated_at timestamp null
-)
-    collate = utf8mb4_unicode_ci;
-
-create table migrations
-(
-    id        int unsigned auto_increment
-        primary key,
-    migration varchar(255) not null,
-    batch     int          not null
-)
-    collate = utf8mb4_unicode_ci;
 
 create table packages
 (
@@ -119,14 +33,7 @@ create table packages
 )
     collate = utf8mb4_unicode_ci;
 
-create table password_reset_tokens
-(
-    email      varchar(255) not null
-        primary key,
-    token      varchar(255) not null,
-    created_at timestamp    null
-)
-    collate = utf8mb4_unicode_ci;
+
 
 create table services
 (
@@ -161,23 +68,7 @@ create table package_items
 )
     collate = utf8mb4_unicode_ci;
 
-create table sessions
-(
-    id            varchar(255)    not null
-        primary key,
-    user_id       bigint unsigned null,
-    ip_address    varchar(45)     null,
-    user_agent    text            null,
-    payload       longtext        not null,
-    last_activity int             not null
-)
-    collate = utf8mb4_unicode_ci;
 
-create index sessions_last_activity_index
-    on sessions (last_activity);
-
-create index sessions_user_id_index
-    on sessions (user_id);
 
 create table staff
 (
